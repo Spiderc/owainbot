f51b50208617a9d705120b5dda1bede544e72a68
Andrew Schleicher <spiderc126@yahoo.com>
2019-06-25 20:37:16 -0500

    Fixed welcome issue, started stubbing out audio

--- src/main/java/si/showdown/owainbot/Constants.java
+++ src/main/java/si/showdown/owainbot/Constants.java
@@ -5,12 +5,11 @@ public class Constants {
 	
 	public static final String BARD_LINK = "https://www.fanfiction.net/s/12528832/1/Birth-and-Re-Death";
 	public static final String EB_LINK = "https://www.fanfiction.net/s/12213521/1/Earthborne";
-	public static final String BENSI_LINK = "https://www.fanfiction.net/s/11403992/1/Metallover-s-Self-Insert-Adventure";
-	public static final String BENREDUX_LINK = "https://www.fanfiction.net/s/12389403/1/Metallover-s-Self-Insert-Adventure-REDUX";
 	
 	public static final String POLL_INTRO = "POLL_USER seeks your advice on naming their weapon! Oh... that's not it?";
 	
-	public static final String HELP = "These are the commands I can run:\n![bard/eb/ben si/ben si redux]:\n\t\tReturns a link to the story.\n!poll question;option1;option2;option3...:\n\t\tCreates a poll with up to ten options.\n!countdown:\n\t\tReturns how long until the release of 3 Houses.\n!source:\n\t\tReturns a link to my source code.";
+	public static final String HELP = "These are the commands I can run:\n![bard/eb]:\n\t\tReturns a link to the story.\n!poll question;option1;option2;option3...:\n\t\tCreates a poll with up to ten options.\n!countdown:\n\t\tReturns how long until the release of 3 Houses.\n!source:\n\t\tReturns a link to my source code.";
+	//\n!play youtubeLink\n\t\tPlays the linked video\n!skip\n\t\tSkips the current song (requires half of the people in vc)
 	
 	public static final String SOURCE = "https://github.com/Spiderc/owainbot";
 	
--- src/main/java/si/showdown/owainbot/ServerInitializer.java
+++ src/main/java/si/showdown/owainbot/ServerInitializer.java
@@ -11,7 +11,10 @@ import java.util.concurrent.ExecutionException;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -48,21 +51,19 @@ public class ServerInitializer implements ApplicationRunner {
 				event.getChannel().sendMessage(Constants.BARD_LINK);
 			} else if (event.getMessageContent().equalsIgnoreCase("!eb")) {
 				event.getChannel().sendMessage(Constants.EB_LINK);
-			} else if (event.getMessageContent().equalsIgnoreCase("!ben si")) {
-				event.getChannel().sendMessage(Constants.BENSI_LINK);
-			} else if (event.getMessageContent().equalsIgnoreCase("!ben si redux")) {
-				event.getChannel().sendMessage(Constants.BENREDUX_LINK);
 			} else if (event.getMessageContent().equalsIgnoreCase("!source")) {
 				event.getChannel().sendMessage(Constants.SOURCE);
 			} else if (event.getMessageContent().equalsIgnoreCase("!countdown")) {
 				event.getChannel().sendMessage(getCountDown());
 			} else if (event.getMessageContent().startsWith("!poll ")) {
 				createPoll(event);
+			} else if (event.getMessageContent().startsWith("!play ")) {
+				play(event);
 			}
 		});
 
 		api.addServerMemberJoinListener(join -> {
-			String message = Constants.GREETINGS[BotUtils.randomNumber(0, Constants.GREETINGS.length) - 1].replaceAll("JOIN_USER", join.getUser().getDisplayName(join.getServer()));
+			String message = Constants.GREETINGS[BotUtils.randomNumber(0, Constants.GREETINGS.length - 1)].replaceAll("JOIN_USER", join.getUser().getDisplayName(join.getServer()));
 			List<ServerTextChannel> channels = join.getServer().getTextChannels();
 			for (ServerTextChannel channel : channels) {
 				if (channel.getName().equalsIgnoreCase("general")) {
@@ -116,5 +117,17 @@ public class ServerInitializer implements ApplicationRunner {
 		int dayCount = Math.toIntExact(difference / (1000 * 60 * 60 * 24));
 		
 		return message.replace("DAYCOUNT", dayCount + "");
+	}
+	
+	private static void play(MessageCreateEvent event) {
+		String link = event.getMessageContent().substring("!play ".length());
+		System.out.println(link);
+		
+		User user = event.getMessageAuthor().asUser().get();
+		Server server = event.getMessage().getServer().get();
+		
+		if(server.getConnectedVoiceChannel(user).isPresent()) {
+			ServerVoiceChannel voiceChannel = event.getMessage().getServer().get().getConnectedVoiceChannel(user).get();
+		} 
 	}
 }
