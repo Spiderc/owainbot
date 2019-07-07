e7533ed377add5f341cf3572419582429636810e
Andrew Schleicher <spiderc126@yahoo.com>
2019-07-07 08:20:44 -0500

    Added !roll

--- src/main/java/si/showdown/owainbot/ServerInitializer.java
+++ src/main/java/si/showdown/owainbot/ServerInitializer.java
@@ -26,22 +26,22 @@ import com.vdurmont.emoji.EmojiParser;
 public class ServerInitializer implements ApplicationRunner {
 	public void run(ApplicationArguments args) throws FileNotFoundException, IOException {
 		boolean prod = true;
-		
+
 		String token = "";
-		if(prod) {
+		if (prod) {
 			Properties properties = new Properties();
 			properties.load(new FileInputStream("webapps/owainbot/WEB-INF/classes/config.properties"));
-			
+
 			token = properties.getProperty("prod");
 		} else {
 			Properties properties = new Properties();
 			properties.load(new FileInputStream("src/main/resources/config.properties"));
-			
+
 			token = properties.getProperty("test");
 		}
 
 		DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
-		
+
 		api.updateActivity("!help for commands");
 
 		api.addMessageCreateListener(event -> {
@@ -57,6 +57,8 @@ public class ServerInitializer implements ApplicationRunner {
 				event.getChannel().sendMessage(getCountDown());
 			} else if (event.getMessageContent().startsWith("!poll ")) {
 				createPoll(event);
+			} else if (event.getMessageContent().startsWith("!roll ")) {
+				rollDice(event);
 			} else if (event.getMessageContent().startsWith("!play ")) {
 				play(event);
 			}
@@ -95,10 +97,56 @@ public class ServerInitializer implements ApplicationRunner {
 			e.printStackTrace();
 		}
 	}
-	
+
+	private static void rollDice(MessageCreateEvent event) {
+		String message = "```";
+		int total = 0;
+
+		String dice = event.getMessageContent().substring("!roll ".length());
+		String[] rolls = dice.replaceAll("\\-", "\\+-").split("\\+");
+
+		for (String roll : rolls) {
+			if(!roll.trim().equals("")) {
+				boolean positive = true;
+				String[] rollParts = roll.trim().replace(" ", "").split("d");
+				
+				if(rollParts[0].startsWith("-") && rollParts.length != 1) {
+					message = message + " - ";
+					positive = false;
+				} else if (!message.equals("```")) {
+					message = message + " + ";
+				}
+
+				if (rollParts.length == 1) {
+					message = message + rollParts[0];
+					total = total + Integer.parseInt(rollParts[0]);
+				} else {
+					message = message + "( ";
+					for (int i = 0; i < Math.abs(Integer.parseInt(rollParts[0])); i++) {
+						int rollValue = BotUtils.randomNumber(1, Integer.parseInt(rollParts[1]));
+						message = message + rollValue + " + ";
+						if(positive) {
+							total = total + rollValue;
+						} else {
+							total = total - rollValue;
+						}
+					}
+					if(!message.equals("( ")) {
+						message = message.substring(0, message.length() - 3);
+					}
+					message = message + " )";
+				}
+			}
+		}
+
+		message = message + " = " + total + "```";
+
+		event.getChannel().sendMessage(message);
+	}
+
 	private static String getCountDown() {
 		String message = Constants.COUNTDOWN;
-		
+
 		Calendar target = Calendar.getInstance();
 		target.set(Calendar.DAY_OF_MONTH, 26);
 		target.set(Calendar.MONTH, 6);
@@ -106,28 +154,28 @@ public class ServerInitializer implements ApplicationRunner {
 		target.set(Calendar.MINUTE, 0);
 		target.set(Calendar.SECOND, 0);
 		target.set(Calendar.MILLISECOND, 0);
-		
+
 		Calendar today = Calendar.getInstance();
 		today.set(Calendar.HOUR_OF_DAY, 0);
 		today.set(Calendar.MINUTE, 0);
 		today.set(Calendar.SECOND, 0);
 		today.set(Calendar.MILLISECOND, 0);
-		
+
 		long difference = target.getTime().getTime() - today.getTime().getTime();
 		int dayCount = Math.toIntExact(difference / (1000 * 60 * 60 * 24));
-		
+
 		return message.replace("DAYCOUNT", dayCount + "");
 	}
-	
+
 	private static void play(MessageCreateEvent event) {
 		String link = event.getMessageContent().substring("!play ".length());
 		System.out.println(link);
-		
+
 		User user = event.getMessageAuthor().asUser().get();
 		Server server = event.getMessage().getServer().get();
-		
-		if(server.getConnectedVoiceChannel(user).isPresent()) {
+
+		if (server.getConnectedVoiceChannel(user).isPresent()) {
 			ServerVoiceChannel voiceChannel = event.getMessage().getServer().get().getConnectedVoiceChannel(user).get();
-		} 
+		}
 	}
 }
