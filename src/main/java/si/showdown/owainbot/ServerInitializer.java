82a8eec2475e0284d69275df2b5bb9b2f831751d
Andrew Schleicher <spiderc126@yahoo.com>
2019-09-05 21:17:03 -0500

    Added !aoa and !census

--- src/main/java/si/showdown/owainbot/ServerInitializer.java
+++ src/main/java/si/showdown/owainbot/ServerInitializer.java
@@ -3,15 +3,25 @@ package si.showdown.owainbot;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Predicate;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.channel.ServerVoiceChannel;
 import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.MessageSet;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
@@ -44,6 +54,15 @@ public class ServerInitializer implements ApplicationRunner {
 
 		api.updateActivity("!help for commands");
 
+		api.addServerTextChannelChangeTopicListener(event -> {
+			try {
+				User user = api.getUserById(116331879635877894L).get();
+				user.sendMessage("The topic in #" + event.getServerTextChannel().get().getName() + " changed from '" + event.getOldTopic() + "' to '" + event.getNewTopic() + "'");
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+		});
+
 		api.addMessageCreateListener(event -> {
 			if (event.getMessageContent().equalsIgnoreCase("!help")) {
 				event.getMessage().getUserAuthor().get().sendMessage(Constants.HELP);
@@ -51,6 +70,8 @@ public class ServerInitializer implements ApplicationRunner {
 				event.getChannel().sendMessage(Constants.BARD_LINK);
 			} else if (event.getMessageContent().equalsIgnoreCase("!eb")) {
 				event.getChannel().sendMessage(Constants.EB_LINK);
+			} else if (event.getMessageContent().equalsIgnoreCase("!aoa")) {
+				event.getChannel().sendMessage(Constants.AOA_LINK);
 			} else if (event.getMessageContent().equalsIgnoreCase("!source")) {
 				event.getChannel().sendMessage(Constants.SOURCE);
 			} else if (event.getMessageContent().equalsIgnoreCase("!roles")) {
@@ -67,6 +88,8 @@ public class ServerInitializer implements ApplicationRunner {
 				addRole(event);
 			} else if (event.getMessageContent().startsWith("!removerole ")) {
 				removeRole(event);
+			} else if (event.getMessageContent().equals("!census")) {
+				census(event);
 			}
 		});
 
@@ -80,51 +103,51 @@ public class ServerInitializer implements ApplicationRunner {
 			}
 		});
 	}
-	
+
 	private static void roles(MessageCreateEvent event) {
 		Server server = event.getMessage().getServer().get();
 		Role botRole = null;
-		
-		for(Role role:event.getApi().getYourself().getRoles(server)) {
-			if(role.isManaged() && !role.isEveryoneRole()) {
+
+		for (Role role : event.getApi().getYourself().getRoles(server)) {
+			if (role.isManaged() && !role.isEveryoneRole()) {
 				botRole = role;
 			}
 		}
-		
+
 		String roles = "```";
-		for(Role role:server.getRoles()) {
-			if(!role.isManaged() && !role.isEveryoneRole()) {
+		for (Role role : server.getRoles()) {
+			if (!role.isManaged() && !role.isEveryoneRole()) {
 				roles = roles + "\n" + role.getName() + ": " + role.getUsers().size();
-			} else if(botRole != null && role == botRole) {
+			} else if (botRole != null && role == botRole) {
 				break;
 			}
 		}
-		
+
 		event.getChannel().sendMessage(roles + "```");
 	}
-	
+
 	private static void attack(MessageCreateEvent event) {
 		User user = event.getMessageAuthor().asUser().get();
 		Server server = event.getMessage().getServer().get();
-		
+
 		boolean foundRole = false;
-		for(Role userRole:user.getRoles(server)) {
-			if(userRole.getName().equals("Black Eagles")) {
+		for (Role userRole : user.getRoles(server)) {
+			if (userRole.getName().equals("Black Eagles")) {
 				event.getChannel().sendMessage("Black Eagle Bash!");
 				foundRole = true;
 				break;
-			} else if(userRole.getName().equals("Blue Lions")) {
+			} else if (userRole.getName().equals("Blue Lions")) {
 				event.getChannel().sendMessage("Blue Lion Lacerate!");
 				foundRole = true;
 				break;
-			} else if(userRole.getName().equals("Golden Deer")) {
+			} else if (userRole.getName().equals("Golden Deer")) {
 				event.getChannel().sendMessage("Golden Deer Gore!");
 				foundRole = true;
 				break;
 			}
 		}
-		
-		if(!foundRole) {
+
+		if (!foundRole) {
 			event.getChannel().sendMessage("My sword hand twitches, but I must know your allegiance before I can show you my latest attack!");
 		}
 	}
@@ -160,11 +183,11 @@ public class ServerInitializer implements ApplicationRunner {
 		String[] rolls = dice.replaceAll("\\-", "\\+-").split("\\+");
 
 		for (String roll : rolls) {
-			if(!roll.trim().equals("")) {
+			if (!roll.trim().equals("")) {
 				boolean positive = true;
 				String[] rollParts = roll.trim().replace(" ", "").split("d");
-				
-				if(rollParts[0].startsWith("-") && rollParts.length != 1) {
+
+				if (rollParts[0].startsWith("-") && rollParts.length != 1) {
 					message = message + " - ";
 					positive = false;
 				} else if (!message.equals("```")) {
@@ -179,13 +202,13 @@ public class ServerInitializer implements ApplicationRunner {
 					for (int i = 0; i < Math.abs(Integer.parseInt(rollParts[0])); i++) {
 						int rollValue = BotUtils.randomNumber(1, Integer.parseInt(rollParts[1]));
 						message = message + rollValue + " + ";
-						if(positive) {
+						if (positive) {
 							total = total + rollValue;
 						} else {
 							total = total - rollValue;
 						}
 					}
-					if(!message.equals("( ")) {
+					if (!message.equals("( ")) {
 						message = message.substring(0, message.length() - 3);
 					}
 					message = message + " )";
@@ -216,15 +239,15 @@ public class ServerInitializer implements ApplicationRunner {
 
 		String role = event.getMessageContent().substring("!addrole ".length());
 		Role targetRole = null;
-		
-		for(Role serverRole:server.getRoles()) {
-			if(serverRole.getName().toLowerCase().equals(role.toLowerCase())) {
+
+		for (Role serverRole : server.getRoles()) {
+			if (serverRole.getName().toLowerCase().equals(role.toLowerCase())) {
 				targetRole = serverRole;
 				break;
 			}
 		}
-		
-		if(targetRole != null) {
+
+		if (targetRole != null) {
 			user.addRole(targetRole);
 		}
 	}
@@ -232,19 +255,80 @@ public class ServerInitializer implements ApplicationRunner {
 	private static void removeRole(MessageCreateEvent event) {
 		User user = event.getMessageAuthor().asUser().get();
 		Server server = event.getMessage().getServer().get();
-		
+
 		String role = event.getMessageContent().substring("!removerole ".length());
 		Role targetRole = null;
-		
-		for(Role userRole:user.getRoles(server)) {
-			if(userRole.getName().toLowerCase().equals(role.toLowerCase())) {
+
+		for (Role userRole : user.getRoles(server)) {
+			if (userRole.getName().toLowerCase().equals(role.toLowerCase())) {
 				targetRole = userRole;
 				break;
 			}
 		}
-		
-		if(targetRole != null) {
+
+		if (targetRole != null) {
 			user.removeRole(targetRole);
 		}
+	}
+
+	private static void census(MessageCreateEvent event) {
+		if (event.getMessage().getUserAuthor().get().getId() == 116331879635877894L) {
+			Server server = event.getMessage().getServer().get();
+
+			Calendar calendar = Calendar.getInstance();
+			calendar.add(Calendar.MONTH, -1);
+
+			Predicate<Message> monthCheck = m -> {
+				return m.getCreationTimestamp().isBefore(calendar.toInstant());
+			};
+
+			Map<String, Integer> messageCounts = new HashMap<>();
+
+			for (ServerTextChannel channel : server.getTextChannels()) {
+				try {
+					MessageSet messages = channel.getMessagesUntil(monthCheck).get();
+					Iterator<Message> iterator = messages.iterator();
+
+					while (iterator.hasNext()) {
+						Message message = iterator.next();
+						if (message.getAuthor().isRegularUser()) {
+							if (messageCounts.containsKey(message.getAuthor().getDisplayName())) {
+								messageCounts.put(message.getAuthor().getDisplayName(), messageCounts.get(message.getAuthor().getDisplayName()) + 1);
+							} else {
+								messageCounts.put(message.getAuthor().getDisplayName(), 1);
+							}
+						}
+					}
+				} catch (InterruptedException | ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+
+			String result = "```In the last month:";
+
+			Map<String, Integer> sortedMap = sortByValue(messageCounts);
+			for (String user : sortedMap.keySet()) {
+				result = result + "\n" + user + " posted " + sortedMap.get(user) + " messages";
+			}
+			result = result + "```";
+
+			event.getChannel().sendMessage(result);
+		}
+	}
+
+	public static HashMap<String, Integer> sortByValue(Map<String, Integer> hm) {
+		List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(hm.entrySet());
+
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+
+		HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+		for (Map.Entry<String, Integer> aa : list) {
+			temp.put(aa.getKey(), aa.getValue());
+		}
+		return temp;
 	}
 }
